242e7abee26822a642b3dc0c839a77f1bc0d950c
Bob Beaty <drbob@themanfromspud.com>
2012-07-06 16:45:00 -0500

    Added More Functions to Base Set
    I wanted to be able to have a decent initial set, so I added in the regular
    arithmetic set (+, -, *, /) and then added in the comparison operators as
    well as the boolean checks (and, or, not) so that we have a pretty decent
    initial set to work with. In order to save code, the comparison and binary
    tests are really just one class, but an ivar tells it how to process the
    data for the type of test it's doing. This makes the code slightly more
    complex, but not too bad, and the savings in lines of code are significant.

--- src/base_functions.cpp
+++ src/base_functions.cpp
@@ -223,7 +223,436 @@ value sum::eval( std::vector<value *> & anArg )
  */
 std::string sum::toString() const
 {
-	return "<sum>";
+	return "<+>";
+}
+
+
+
+
+
+
+/**
+ * diff() - this function simply differences the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+/*******************************************************************
+ *
+ *                       Evaluation Methods
+ *
+ *******************************************************************/
+/**
+ * This is the main evaluation point for the function. It takes a
+ * vector of values and returns a value - simple. How it does this
+ * is entirely up to the developer of that function.
+ */
+value diff::eval( std::vector<value *> & anArg )
+{
+	// get the number of arguments to check
+	size_t		sz = anArg.size();
+	/**
+	 * Get the initial value as the first, defined, arg - if we
+	 * have ANY - we just have to find it.
+	 */
+	value		ans;
+	size_t		pos = 0;
+	while ((pos < sz) && (anArg[pos] == NULL)) {
+		++pos;
+	}
+	// if we have something, then get it, and process the rest
+	if (pos < sz) {
+		ans = *(anArg[pos++]);
+		// check for unary minus
+		if (pos >= sz) {
+			// unary minus - just negate what we have
+			ans *= -1;
+		} else {
+			// run through all the rest, comparing as we go
+			value	*v = NULL;
+			while (pos < sz) {
+				// get the pointer to the next argument in the list
+				v = anArg[pos];
+				// simply difference all the valid values as best we can...
+				if ((v != NULL) && !v->isUndefined()) {
+					ans -= *v;
+				}
+				++pos;
+			}
+		}
+	}
+	return ans;
+}
+
+
+/*******************************************************************
+ *
+ *                         Utility Methods
+ *
+ *******************************************************************/
+/**
+ * There are a lot of times that a human-readable version of
+ * this instance will come in handy. This is that method. It's
+ * not necessarily meant to be something to process, but most
+ * likely what a debugging system would want to write out for
+ * this guy.
+ */
+std::string diff::toString() const
+{
+	return "<->";
+}
+
+
+
+
+
+
+/**
+ * prod() - this function simply multiplies the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+/*******************************************************************
+ *
+ *                       Evaluation Methods
+ *
+ *******************************************************************/
+/**
+ * This is the main evaluation point for the function. It takes a
+ * vector of values and returns a value - simple. How it does this
+ * is entirely up to the developer of that function.
+ */
+value prod::eval( std::vector<value *> & anArg )
+{
+	// get the number of arguments to check
+	size_t		sz = anArg.size();
+	/**
+	 * Get the initial value as the first, defined, arg - if we
+	 * have ANY - we just have to find it.
+	 */
+	value		ans;
+	size_t		pos = 0;
+	while ((pos < sz) && (anArg[pos] == NULL)) {
+		++pos;
+	}
+	// if we have something, then get it, and process the rest
+	if (pos < sz) {
+		ans = *(anArg[pos++]);
+		// run through all the rest, comparing as we go
+		value	*v = NULL;
+		while (pos < sz) {
+			// get the pointer to the next argument in the list
+			v = anArg[pos];
+			// simply multiply all the valid values as best we can...
+			if ((v != NULL) && !v->isUndefined()) {
+				ans *= *v;
+			}
+			++pos;
+		}
+	}
+	return ans;
+}
+
+
+/*******************************************************************
+ *
+ *                         Utility Methods
+ *
+ *******************************************************************/
+/**
+ * There are a lot of times that a human-readable version of
+ * this instance will come in handy. This is that method. It's
+ * not necessarily meant to be something to process, but most
+ * likely what a debugging system would want to write out for
+ * this guy.
+ */
+std::string prod::toString() const
+{
+	return "<*>";
+}
+
+
+
+
+
+
+/**
+ * quot() - this function simply divides the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+/*******************************************************************
+ *
+ *                       Evaluation Methods
+ *
+ *******************************************************************/
+/**
+ * This is the main evaluation point for the function. It takes a
+ * vector of values and returns a value - simple. How it does this
+ * is entirely up to the developer of that function.
+ */
+value quot::eval( std::vector<value *> & anArg )
+{
+	// get the number of arguments to check
+	size_t		sz = anArg.size();
+	/**
+	 * Get the initial value as the first, defined, arg - if we
+	 * have ANY - we just have to find it.
+	 */
+	value		ans;
+	size_t		pos = 0;
+	while ((pos < sz) && (anArg[pos] == NULL)) {
+		++pos;
+	}
+	// if we have something, then get it, and process the rest
+	if (pos < sz) {
+		ans = *(anArg[pos++]);
+		// run through all the rest, comparing as we go
+		value	*v = NULL;
+		while (pos < sz) {
+			// get the pointer to the next argument in the list
+			v = anArg[pos];
+			// simply divide all the valid values as best we can...
+			if ((v != NULL) && !v->isUndefined()) {
+				ans /= *v;
+			}
+			++pos;
+		}
+	}
+	return ans;
+}
+
+
+/*******************************************************************
+ *
+ *                         Utility Methods
+ *
+ *******************************************************************/
+/**
+ * There are a lot of times that a human-readable version of
+ * this instance will come in handy. This is that method. It's
+ * not necessarily meant to be something to process, but most
+ * likely what a debugging system would want to write out for
+ * this guy.
+ */
+std::string quot::toString() const
+{
+	return "</>";
+}
+
+
+
+
+
+
+/**
+ * comp() - this function does all the equality/inequality functions for
+ *          the values of the arguments based on the constructor argument.
+ *          The default is '=='. The comparisons are all done on the
+ *          values themselves.
+ */
+/*******************************************************************
+ *
+ *                       Evaluation Methods
+ *
+ *******************************************************************/
+/**
+ * This is the main evaluation point for the function. It takes a
+ * vector of values and returns a value - simple. How it does this
+ * is entirely up to the developer of that function.
+ */
+value comp::eval( std::vector<value *> & anArg )
+{
+	// get the number of arguments to check
+	size_t		sz = anArg.size();
+	/**
+	 * Get the initial value as the first, defined, arg - if we
+	 * have ANY - we just have to find it.
+	 */
+	size_t		pos = 0;
+	while ((pos < sz) && (anArg[pos] == NULL)) {
+		++pos;
+	}
+	// if we have something, then get it, and process the rest
+	bool		comp = true;
+	if (pos < sz) {
+		value	first = *(anArg[pos++]);
+		// run through all the rest, comparing as we go
+		value	*v = NULL;
+		bool	keepGoing = true;
+		while (keepGoing && (pos < sz)) {
+			// get the pointer to the next argument in the list
+			v = anArg[pos];
+			// simply check all the valid values as best we can...
+			if ((v != NULL) && !v->isUndefined()) {
+				// based on what we are, check for show stopper
+				switch (_type) {
+					case eEquals :
+						if (first != (const value &)*v) {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+					case eNotEquals :
+						if (first == (const value &)*v) {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+					case eLessThan :
+						if (first < (const value &)*v) {
+							// compare against this value now
+							first = *v;
+						} else {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+					case eGreaterThan :
+						if (first > (const value &)*v) {
+							// compare against this value now
+							first = *v;
+						} else {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+					case eLessOrEqual :
+						if (first <= (const value &)*v) {
+							// compare against this value now
+							first = *v;
+						} else {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+					case eGreaterOrEqual :
+						if (first >= (const value &)*v) {
+							// compare against this value now
+							first = *v;
+						} else {
+							comp = false;
+							keepGoing = false;
+						}
+						break;
+				}
+			}
+			++pos;
+		}
+	}
+	return comp;
+}
+
+
+/*******************************************************************
+ *
+ *                         Utility Methods
+ *
+ *******************************************************************/
+/**
+ * There are a lot of times that a human-readable version of
+ * this instance will come in handy. This is that method. It's
+ * not necessarily meant to be something to process, but most
+ * likely what a debugging system would want to write out for
+ * this guy.
+ */
+std::string comp::toString() const
+{
+	switch (_type) {
+		case eEquals :			return "<.eq.>"; break;
+		case eNotEquals :		return "<.ne.>"; break;
+		case eLessThan :		return "<.lt.>"; break;
+		case eGreaterThan :		return "<.gt.>"; break;
+		case eLessOrEqual :		return "<.le.>"; break;
+		case eGreaterOrEqual :	return "<.ge.>"; break;
+	}
+	return "<.eq.>";
+}
+
+
+
+
+
+
+/**
+ * bin() - this function does all the binary functions for the values
+ *         of the arguments based on the constructor argument.
+ *         The default is 'and'. The comparisons are all done on the
+ *         values themselves.
+ */
+/*******************************************************************
+ *
+ *                       Evaluation Methods
+ *
+ *******************************************************************/
+/**
+ * This is the main evaluation point for the function. It takes a
+ * vector of values and returns a value - simple. How it does this
+ * is entirely up to the developer of that function.
+ */
+value bin::eval( std::vector<value *> & anArg )
+{
+	// get the number of arguments to check
+	size_t		sz = anArg.size();
+	size_t		pos = 0;
+	// assume the test is true, and check it
+	bool		test = true;
+	if (pos < sz) {
+		// run through all the values, checking as we go
+		value	*v = NULL;
+		bool	keepGoing = true;
+		while (keepGoing && (pos < sz)) {
+			// get the pointer to the next argument in the list
+			v = anArg[pos];
+			// simply check all the valid values as best we can...
+			if ((v != NULL) && !v->isUndefined()) {
+				// based on what we are, check for show stopper
+				switch (_type) {
+					case eAnd :
+						if (!v->evalAsBool()) {
+							test = false;
+							keepGoing = false;
+						}
+						break;
+					case eOr :
+						if (v->evalAsBool()) {
+							test = true;
+							keepGoing = false;
+						}
+						break;
+					case eNot :
+						// this is a unary operator
+						test = !v->evalAsBool();
+						keepGoing = false;
+						break;
+				}
+			}
+			++pos;
+		}
+	}
+	return test;
+}
+
+
+/*******************************************************************
+ *
+ *                         Utility Methods
+ *
+ *******************************************************************/
+/**
+ * There are a lot of times that a human-readable version of
+ * this instance will come in handy. This is that method. It's
+ * not necessarily meant to be something to process, but most
+ * likely what a debugging system would want to write out for
+ * this guy.
+ */
+std::string bin::toString() const
+{
+	switch (_type) {
+		case eAnd :		return "<.and.>"; break;
+		case eOr :		return "<.or.>"; break;
+		case eNot :		return "<.not.>"; break;
+	}
+	return "<.and.>";
 }
 }		// end of namespace func
 }		// end of namespace lkit
--- src/base_functions.h
+++ src/base_functions.h
@@ -46,6 +46,7 @@ class max :
 		 */
 		max() { };
 		max( const max & anOther ) { };
+		virtual function *clone() const { return new max(*this); }
 		virtual ~max() { };
 		max & operator=( const max & anOther ) { return *this; };
 
@@ -98,6 +99,7 @@ class min :
 		 */
 		min() { };
 		min( const min & anOther ) { };
+		virtual function *clone() const { return new min(*this); }
 		virtual ~min() { };
 		min & operator=( const min & anOther ) { return *this; };
 
@@ -150,6 +152,7 @@ class sum :
 		 */
 		sum() { };
 		sum( const sum & anOther ) { };
+		virtual function *clone() const { return new sum(*this); }
 		virtual ~sum() { };
 		sum & operator=( const sum & anOther ) { return *this; };
 
@@ -178,6 +181,312 @@ class sum :
 		 * this guy.
 		 */
 		virtual std::string toString() const;
+};
+
+
+
+/**
+ * diff() - this function simply differences the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+class diff :
+	public function
+{
+	public:
+		/*******************************************************************
+		 *
+		 *                     Constructors/Destructor
+		 *
+		 *******************************************************************/
+		/**
+		 * This function has no state, so these are all very simple to
+		 * stub out here in the header.
+		 */
+		diff() { };
+		diff( const diff & anOther ) { };
+		virtual function *clone() const { return new diff(*this); }
+		virtual ~diff() { };
+		diff & operator=( const diff & anOther ) { return *this; };
+
+		/*******************************************************************
+		 *
+		 *                       Evaluation Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * This is the main evaluation point for the function. It takes a
+		 * vector of values and returns a value - simple. How it does this
+		 * is entirely up to the developer of that function.
+		 */
+		virtual value eval( std::vector<value *> & anArg );
+
+		/*******************************************************************
+		 *
+		 *                         Utility Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * There are a lot of times that a human-readable version of
+		 * this instance will come in handy. This is that method. It's
+		 * not necessarily meant to be something to process, but most
+		 * likely what a debugging system would want to write out for
+		 * this guy.
+		 */
+		virtual std::string toString() const;
+};
+
+
+
+/**
+ * prod() - this function simply multiplies the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+class prod :
+	public function
+{
+	public:
+		/*******************************************************************
+		 *
+		 *                     Constructors/Destructor
+		 *
+		 *******************************************************************/
+		/**
+		 * This function has no state, so these are all very simple to
+		 * stub out here in the header.
+		 */
+		prod() { };
+		prod( const prod & anOther ) { };
+		virtual function *clone() const { return new prod(*this); }
+		virtual ~prod() { };
+		prod & operator=( const prod & anOther ) { return *this; };
+
+		/*******************************************************************
+		 *
+		 *                       Evaluation Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * This is the main evaluation point for the function. It takes a
+		 * vector of values and returns a value - simple. How it does this
+		 * is entirely up to the developer of that function.
+		 */
+		virtual value eval( std::vector<value *> & anArg );
+
+		/*******************************************************************
+		 *
+		 *                         Utility Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * There are a lot of times that a human-readable version of
+		 * this instance will come in handy. This is that method. It's
+		 * not necessarily meant to be something to process, but most
+		 * likely what a debugging system would want to write out for
+		 * this guy.
+		 */
+		virtual std::string toString() const;
+};
+
+
+
+/**
+ * quot() - this function simply divides the values of the arguments
+ *          and returns it. The operations are all done on the
+ *          values themselves.
+ */
+class quot :
+	public function
+{
+	public:
+		/*******************************************************************
+		 *
+		 *                     Constructors/Destructor
+		 *
+		 *******************************************************************/
+		/**
+		 * This function has no state, so these are all very simple to
+		 * stub out here in the header.
+		 */
+		quot() { };
+		quot( const quot & anOther ) { };
+		virtual function *clone() const { return new quot(*this); }
+		virtual ~quot() { };
+		quot & operator=( const quot & anOther ) { return *this; };
+
+		/*******************************************************************
+		 *
+		 *                       Evaluation Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * This is the main evaluation point for the function. It takes a
+		 * vector of values and returns a value - simple. How it does this
+		 * is entirely up to the developer of that function.
+		 */
+		virtual value eval( std::vector<value *> & anArg );
+
+		/*******************************************************************
+		 *
+		 *                         Utility Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * There are a lot of times that a human-readable version of
+		 * this instance will come in handy. This is that method. It's
+		 * not necessarily meant to be something to process, but most
+		 * likely what a debugging system would want to write out for
+		 * this guy.
+		 */
+		virtual std::string toString() const;
+};
+
+
+
+/**
+ * comp() - this function does all the equality/inequality functions for
+ *          the values of the arguments based on the constructor argument.
+ *          The default is '=='. The comparisons are all done on the
+ *          values themselves.
+ */
+class comp :
+	public function
+{
+	public:
+		/**
+		 * These are the different types of comparisons that this function
+		 * can perform. The default, no-arg constructor is eEquals, but the
+		 * caller can create any one they desire.
+		 */
+		enum comp_type {
+			eEquals = 0,
+			eNotEquals,
+			eLessThan,
+			eGreaterThan,
+			eLessOrEqual,
+			eGreaterOrEqual
+		};
+		/*******************************************************************
+		 *
+		 *                     Constructors/Destructor
+		 *
+		 *******************************************************************/
+		/**
+		 * This function has no state, so these are all very simple to
+		 * stub out here in the header.
+		 */
+		comp() : _type(eEquals) { };
+		comp( comp_type aType ) : _type(aType) { };
+		comp( const comp & anOther ) : _type(anOther._type) { };
+		virtual function *clone() const { return new comp(*this); }
+		virtual ~comp() { };
+		comp & operator=( const comp & anOther )
+		{
+			_type = anOther._type;
+			return *this;
+		};
+
+		/*******************************************************************
+		 *
+		 *                       Evaluation Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * This is the main evaluation point for the function. It takes a
+		 * vector of values and returns a value - simple. How it does this
+		 * is entirely up to the developer of that function.
+		 */
+		virtual value eval( std::vector<value *> & anArg );
+
+		/*******************************************************************
+		 *
+		 *                         Utility Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * There are a lot of times that a human-readable version of
+		 * this instance will come in handy. This is that method. It's
+		 * not necessarily meant to be something to process, but most
+		 * likely what a debugging system would want to write out for
+		 * this guy.
+		 */
+		virtual std::string toString() const;
+	private:
+		// this is the type of comparison this instance is doing
+		comp_type		_type;
+};
+
+
+
+/**
+ * bin() - this function does all the binary functions for the values
+ *         of the arguments based on the constructor argument.
+ *         The default is 'and'. The comparisons are all done on the
+ *         values themselves.
+ */
+class bin :
+	public function
+{
+	public:
+		/**
+		 * These are the different types of operations that this function
+		 * can perform. The default, no-arg constructor is eAnd, but the
+		 * caller can create any one they desire.
+		 */
+		enum bin_type {
+			eAnd = 0,
+			eOr,
+			eNot
+		};
+		/*******************************************************************
+		 *
+		 *                     Constructors/Destructor
+		 *
+		 *******************************************************************/
+		/**
+		 * This function has no state, so these are all very simple to
+		 * stub out here in the header.
+		 */
+		bin() : _type(eAnd) { };
+		bin( bin_type aType ) : _type(aType) { };
+		bin( const bin & anOther ) : _type(anOther._type) { };
+		virtual function *clone() const { return new bin(*this); }
+		virtual ~bin() { };
+		bin & operator=( const bin & anOther )
+		{
+			_type = anOther._type;
+			return *this;
+		};
+
+		/*******************************************************************
+		 *
+		 *                       Evaluation Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * This is the main evaluation point for the function. It takes a
+		 * vector of values and returns a value - simple. How it does this
+		 * is entirely up to the developer of that function.
+		 */
+		virtual value eval( std::vector<value *> & anArg );
+
+		/*******************************************************************
+		 *
+		 *                         Utility Methods
+		 *
+		 *******************************************************************/
+		/**
+		 * There are a lot of times that a human-readable version of
+		 * this instance will come in handy. This is that method. It's
+		 * not necessarily meant to be something to process, but most
+		 * likely what a debugging system would want to write out for
+		 * this guy.
+		 */
+		virtual std::string toString() const;
+	private:
+		// this is the type of operation this instance is doing
+		bin_type			_type;
 };
 }		// end of namespace func
 }		// end of namespace lkit
